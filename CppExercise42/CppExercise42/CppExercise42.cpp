80b14a20d799bf45a4488c1cf27a7c4372373328
Beyah96 <bahsaid96@gmail.com>
2024-09-01 19:05:30 +0200

    Task Duration in Seconds

---
+++ CppExercise42/CppExercise42/CppExercise42.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	short Days, Hours, Minutes, Seconds;
+
+	cout << "Enter the number of days : ";
+	cin >> Days;
+
+	cout << "Enter the number of hours : ";
+	cin >> Hours;
+
+	cout << "Enter the number of minutes : ";
+	cin >> Minutes;
+
+	cout << "Enter the number of seconds : ";
+	cin >> Seconds;
+
+	int NumberOfHours = Hours + 24 * Days, NumberOfMinutes = Minutes + 60 * NumberOfHours, NumberOfSeconds = Seconds + 60 * NumberOfMinutes;
+
+	cout << "Total number of seconds : " << NumberOfSeconds;
+
+	return 0;
+}
