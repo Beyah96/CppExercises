b141a8aae11a5385f9559b82488d72328b6f1e13
Beyah96 <bahsaid96@gmail.com>
2024-09-01 15:20:03 +0200

    exercise 15: rectangle area

---
+++ CppExercise15/CppExercise15/CppExercise15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int length, width;
+
+	cout << "Enter the length of the rectangle : ";
+	cin >> length;
+
+	cout << "Enter the width : ";
+	cin >> width;
+
+	cout << "The area of your rectangle is : " << length * width;
+
+	return 0;
+}
