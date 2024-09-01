3b3a0b7ddf3186df846c3e9f5266fec41e717f64
Beyah96 <bahsaid96@gmail.com>
2024-09-01 14:53:16 +0200

    exercises 7 and 9

---
+++ CppExercise7/CppExercise7/CppExercise7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	float Number; 
+
+	cout << "Please enter a number : ";
+	cin >> Number;
+
+	cout << "The half of " << Number << " is " << Number / 2;
+
+	return 0;
+}
---
+++ CppExercise9/CppExercise9/CppExercise9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std; 
+
+
+int main() {
+
+	int Number1, Number2, Number3;
+
+	cout << "Enter the first number : ";
+	cin >> Number1;
+
+	cout << "Enter the second number : ";
+	cin >> Number2;
+
+	cout << "Enter the third number : ";
+	cin >> Number3;
+
+	cout << "The sum of entred numbers is : " << Number1 + Number2 + Number3;
+
+	return 0;
+
+}
