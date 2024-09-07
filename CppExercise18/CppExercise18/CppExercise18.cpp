f841d399829af1eb45ebf522843b8eb54a8c7f49
Beyah96 <bahsaid96@gmail.com>
2024-09-07 16:13:01 +0200

    Add a new function

--- CppExercise18/CppExercise18/CppExercise18.cpp
+++ CppExercise18/CppExercise18/CppExercise18.cpp
@@ -3,17 +3,21 @@
 
 using namespace std;
 
+float CalculeCircleArea( float radius) {
+	const float PI = 3.14;
+	return PI * pow(radius, 2);
+}
+
 int main() {
 
 	float radius;
-	const float PI = 3.14;
+	
 
 	cout << "Please enter the radius of your circle : ";
 	cin >> radius;
 
-	float area = PI * pow(radius, 2);
 
-	cout << "The area of your circle is : " << ceil(area);
+	cout << "The area of your circle is : " << ceil(CalculeCircleArea(radius));
 
 	return 0;
 }
