78af8a97e283e8c014bcafacd152650b7ebb209f
Beyah96 <bahsaid96@gmail.com>
2024-09-07 19:13:57 +0200

    Add a new function that calculate the number of days, hours, minutes from a Number of seconds

--- CppExercise43/CppExercise43/CppExercise43.cpp
+++ CppExercise43/CppExercise43/CppExercise43.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-
-	int NumberOfSecond, SecondPerMinute = 60, SecondPerHour = 60 * SecondPerMinute, SecondPerDay = 24 * SecondPerHour;
-
-	cout << "Enter the number of seconds : ";
-	cin >> NumberOfSecond;
+void GetdateDetails(int NumberOfSecond) {
+	int SecondPerMinute = 60, SecondPerHour = 60 * SecondPerMinute, SecondPerDay = 24 * SecondPerHour;
 
 	int NumberOfDays = NumberOfSecond / SecondPerDay, Remainder = NumberOfSecond % SecondPerDay;
-	
+
 	int NumberOfHours = Remainder / SecondPerHour;
 	Remainder = Remainder % SecondPerHour;
-	
+
 	int NumberOfMinutes = Remainder / SecondPerMinute;
 	Remainder = Remainder % SecondPerMinute;
 
 	cout << NumberOfDays << " : " << NumberOfHours << " : " << NumberOfMinutes << " : " << Remainder;
-	return 0;
+
+}
+int main() {
+
+	int NumberOfSecond;
+
+	cout << "Enter the number of seconds : ";
+	cin >> NumberOfSecond;
+	GetdateDetails(NumberOfSecond);
+return 0;
 }
