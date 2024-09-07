46827a5047b8bf096cc4d9b93c3943d14016d087
Beyah96 <bahsaid96@gmail.com>
2024-09-07 17:05:20 +0200

    rewriting a the solution by using a new function that calculate the power of a given number and display it

--- CppExercise31/CppExercise31/CppExercise31.cpp
+++ CppExercise31/CppExercise31/CppExercise31.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+void GetPowers1_2_3(int Number) {
+	cout << "Power of 2, 3 and 4 of your number are: \n" << round(pow(Number, 2)) << endl << round(pow(Number, 3)) << endl << round(pow(Number, 4));
+
+}
+
 int main() {
 
 	int Number;
@@ -10,8 +15,7 @@ int main() {
 	cout << "Enter a Number: ";
 	cin >> Number;
 
-
-	cout << "Power of 2, 3 and 4 of your number are: \n" << round(pow(Number, 2)) << endl << round(pow(Number, 3)) << endl << round(pow(Number, 4));
-
+	GetPowers1_2_3(Number);
+	
 	return 0;
 }
