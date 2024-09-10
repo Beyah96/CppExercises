4adc646b74acfcc939d6b7fbd50d6bb9723ebecb
Beyah96 <bahsaid96@gmail.com>
2024-09-10 23:59:57 +0200

    Sum even numbers from 1 to N

---
+++ CppExercise29/CppExercise29/CppExercise29.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNumber() {
+	int N;
+	cout << "Enter your number : ";
+	cin >> N;
+	return N;
+}
+int ReturnOddNumber(int N) {
+	if (N % 2 == 0) {
+		return N;
+	}
+	else {
+		return 0;
+	}
+}
+int SumNumbersBefore(int N) {
+	int Sum = 0;
+	for (int i = 1; i <= N; i++) {
+		Sum += ReturnOddNumber(i);
+	}
+	return Sum;
+
+}
+int main() {
+	cout << "The sum of all numbers before your number with your number is : " << SumNumbersBefore(ReadNumber());
+	return 0;
+}
