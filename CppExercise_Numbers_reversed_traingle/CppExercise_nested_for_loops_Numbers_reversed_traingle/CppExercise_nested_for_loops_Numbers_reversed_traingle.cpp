fdbf5fd4a27596a11336c97b855ebc56ab5f7e66
Beyah96 <bahsaid96@gmail.com>
2024-09-15 00:23:18 +0200

    nested for loops : numbers reversed triangle

---
+++ CppExercise_Numbers_reversed_traingle/CppExercise_nested_for_loops_Numbers_reversed_traingle/CppExercise_nested_for_loops_Numbers_reversed_traingle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << j;
+		}
+		cout << "\n";
+	}
+	return 0;
+}
