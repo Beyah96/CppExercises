5739fe568963bdc8d1c858ad9d8f9cfc54168dc6
Beyah96 <bahsaid96@gmail.com>
2024-09-15 00:06:52 +0200

    Exercise 32 with while loop

---
+++ CppExercise32_with_while_loop/CppExercise32_with_while_loop/CppExercise32_with_while_loop.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNumber() {
+	int Number;
+	cout << "Please enter your chosen number : ";
+	cin >> Number;
+	return Number;
+}
+
+int ReadPower() {
+	int Power;
+	cout << "Please enter your chosen Power : ";
+	cin >> Power;
+	return Power;
+}
+int GetPower(int N, int P) {
+	int Powered = 1, i = 1;
+
+	if (P != 0) {
+		while (i <= P) {
+			Powered *= N;
+			i++;
+		}
+	}
+	return Powered;
+}
+
+int main() {
+	int Number, Power;
+	Number = ReadNumber();
+	Power = ReadPower();
+	cout << "THe " << Number << "^" << Power << " = " << GetPower(Number, Power);
+
+	return 0;
+}
